c09b7424254b5d822fd9c7603f2bbe827080273a
Ethan <81447958+soulfire-singularity@users.noreply.github.com>
2024-06-23 16:36:16 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    const name = $('#name').val();
+    const email = $('#email').val();
+    const message = $('#message').val();
+    alert(`Thank you, ${name}! Your message has been received.`);
+    // You can add additional form handling here
+  });
+});
